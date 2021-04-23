4e6acddaec9a873ccd0f59dd82641aac8d82e3f0
Gaël Lebrun <gael.lebrun@myskillcamp.com>
2021-04-23 21:53:06 +0200

    initial release

---
+++ src/timestamphmac-auth.js
@@ -0,0 +1,42 @@
+const {smartEncodeUrl} = require('insomnia-url');
+const crypto = require('crypto');
+
+module.exports = function (context) {
+  const commaDecodedUrl = smartEncodeUrl(context.request.getUrl(), true);
+
+  const hmac = context.request.getEnvironmentVariable('hmac');
+
+  if(!hmac){
+    return
+  }
+
+  try {
+    var signRequest = false
+    
+    const appliesTo = hmac.appliesTo ? hmac.appliesTo : [ "sapiti.ovh", "sapiti.net" ]
+    appliesTo.forEach((it) => {
+      if(commaDecodedUrl.includes(it)){
+        signRequest = true
+        return 
+      }
+    })
+    if(!signRequest){
+      return
+    }
+
+    const bodyText = context.request.getBodyText();
+    const json = JSON.parse(bodyText? bodyText : '{}');
+    const hash = crypto.createHmac('sha256', hmac.privateKey);
+
+    json.timestamp = moment().format();
+    json.publickey = hmac.publicKey
+    hash.update(json.publickey + json.timestamp, 'utf8');
+    json.signature = hash.digest('hex')
+
+    context.request.setBodyText(JSON.stringify(json));
+
+  } catch (err) {
+    alert(err.message);
+    throw Error(err)
+  }
+};
